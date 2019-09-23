0c6c08840cbe82c4503c8fca8e9e8cfc8b148712
Russell <russell@union-pos.com>
2019-09-23 10:42:15 -0500

    unionpos changes

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,4 +1,4 @@
 output "result" {
-  value       = "${module.aggregated_policy.result_document}"
+  value       = module.aggregated_policy.result_document
   description = "Aggregated policy document"
 }
